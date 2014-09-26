bfd12e755f9be0b8ef46f99e12afc22d6573fc4e
Jon Pretty <jon.pretty@propensive.com>
2014-09-26 23:04:54 +0200

    Version/dependency tweaks

--- project/project.scala
+++ project/project.scala
@@ -5,22 +5,16 @@ object project extends ProjectSettings {
   def description = "Tests for Rapture JSON"
   
   def dependencies = Seq(
-    "core" -> "0.10.0",
-    "test" -> "0.10.0",
-    "data" -> "0.10.1",
-    "json" -> "0.10.1",
-    "json-jawn" -> "0.10.0",
-    "json-lift" -> "0.10.0",
-    "json-jackson" -> "0.10.0",
-    "json-json4s" -> "0.10.0",
-    "json-spray" -> "0.10.0",
-    "json-argonaut" -> "0.10.0"
+    "test" -> "0.10.1",
+    "json-jawn" -> "1.0.0",
+    "json-lift" -> "1.0.0",
+    "json-jackson" -> "1.0.0",
+    "json-json4s" -> "1.0.0",
+    "json-spray" -> "1.0.0",
+    "json-argonaut" -> "1.0.0"
   )
   
   def thirdPartyDependencies = Seq(
-    ("org.json4s", "json4s-native_2.10", "3.2.9"),
-    ("org.spire-math", "jawn-ast_2.10", "0.6.0"),
-    ("org.spire-math", "jawn-parser_2.10", "0.6.0")
   )
 
   def imports = Seq(
