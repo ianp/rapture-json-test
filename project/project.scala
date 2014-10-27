56d76d8654d1d8c75d9ec4455774f359b8bf736a
Jon Pretty <jon.pretty@propensive.com>
2014-10-27 11:55:33 +0000

    Updated versions

--- project/project.scala
+++ project/project.scala
@@ -1,17 +1,17 @@
 object project extends ProjectSettings {
   def scalaVersion = "2.10.4"
-  def version = "1.0.4"
+  def version = "1.0.5"
   def name = "json-test"
   def description = "Tests for Rapture JSON"
   
   def dependencies = Seq(
     "test" -> "0.10.1",
-    "json-jawn" -> "1.0.4",
-    "json-lift" -> "1.0.4",
-    "json-jackson" -> "1.0.4",
-    "json-json4s" -> "1.0.4",
-    "json-spray" -> "1.0.4",
-    "json-argonaut" -> "1.0.4"
+    "json-jawn" -> "1.0.5",
+    "json-lift" -> "1.0.5",
+    "json-jackson" -> "1.0.5",
+    "json-json4s" -> "1.0.5",
+    "json-spray" -> "1.0.5",
+    "json-argonaut" -> "1.0.5"
   )
   
   def thirdPartyDependencies = Seq(
