aa644ec21d232634e9a3f0a14e3032e356288b08
Jon Pretty <jon.pretty@propensive.com>
2014-11-17 17:53:39 -0800

    Removed lift from generate.sh script

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-for BACKEND in scalaJson spray jawn json4s lift argonaut; do
+for BACKEND in scalaJson spray play jawn json4s lift argonaut; do
   sed "s/BACKEND/$BACKEND/g" pre/json.scala > src/$BACKEND.scala
 done
