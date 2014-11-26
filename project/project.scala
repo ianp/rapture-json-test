264d7df1e47261f3fec286a6948f199b30a683e8
Jon Pretty <jon.pretty@propensive.com>
2014-11-26 23:31:11 +0000

    Use 1.0.8 version of Rapture JSON

--- project/project.scala
+++ project/project.scala
@@ -1,18 +1,18 @@
 object project extends ProjectSettings {
   def scalaVersion = "2.10.4"
-  def version = "1.0.7"
+  def version = "1.0.8"
   def name = "json-test"
   def description = "Tests for Rapture JSON"
   
   def dependencies = Seq(
     "test" -> "0.10.1",
-    "json-argonaut" -> "1.0.7",
-    "json-jackson" -> "1.0.7",
-    "json-jawn" -> "1.0.7",
-    "json-json4s" -> "1.0.7",
-    "json-spray" -> "1.0.7",
-    "json-play" -> "1.0.7",
-    "json" -> "1.0.7",
+    "json-argonaut" -> "1.0.8",
+    "json-jackson" -> "1.0.8",
+    "json-jawn" -> "1.0.8",
+    "json-json4s" -> "1.0.8",
+    "json-spray" -> "1.0.8",
+    "json-play" -> "1.0.8",
+    "json" -> "1.0.8",
     "log" -> "0.1.0"
   )
   
