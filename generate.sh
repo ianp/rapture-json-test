74d23ebedda5a8b6900f576e9fc4e9059af1a141
Jon Pretty <jon.pretty@propensive.com>
2015-02-17 20:32:29 +0000

    Deletions

--- generate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-for BACKEND in scalaJson spray play jawn json4s argonaut; do
-  sed "s/BACKEND/$BACKEND/g" pre/json.scala > src/$BACKEND.scala
-done
--- project/project.scala
+++ project/project.scala
@@ -1,19 +1,20 @@
 object project extends ProjectSettings {
-  def scalaVersion = "2.10.4"
-  def version = "1.0.8"
+  def scalaVersion = "2.11.4"
+  def version = "1.1.0"
   def name = "json-test"
   def description = "Tests for Rapture JSON"
   
   def dependencies = Seq(
-    "test" -> "0.10.1",
-    "json-argonaut" -> "1.0.8",
-    "json-jackson" -> "1.0.8",
-    "json-jawn" -> "1.0.8",
-    "json-json4s" -> "1.0.8",
-    "json-spray" -> "1.0.8",
-    "json-play" -> "1.0.8",
-    "json" -> "1.0.8",
-    "log" -> "0.1.0"
+    "test" -> "1.1.0",
+    "json-argonaut" -> "1.1.0",
+    "json-jackson" -> "1.1.0",
+    "json-jawn" -> "1.1.0",
+    //"json-lift" -> "1.1.0",
+    "json-json4s" -> "1.1.0",
+    "json-spray" -> "1.1.0",
+    "json-play" -> "1.1.0",
+    "json" -> "1.1.0"
+//    "log" -> "0.1.0"
   )
   
   def thirdPartyDependencies = Seq(
